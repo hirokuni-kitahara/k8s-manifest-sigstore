0fef332d3fda984a11997dfc98996564a321d3a0
hirokuni-kitahara <hirokuni.kitahara1@ibm.com>
2023-02-14 11:34:27 +0900

    add github oidc test
    Signed-off-by: hirokuni-kitahara <hirokuni.kitahara1@ibm.com>

---
+++ test/github_oidc_test.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Copyright 2023 The Sigstore Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"";
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+: "${GIT_HASH:?Environment variable empty or not defined.}"
+: "${GITHUB_RUN_ID:?Environment variable empty or not defined.}"
+: "${GITHUB_RUN_ATTEMPT:?Environment variable empty or not defined.}"
+: "${TEST_IMAGE:?Environment variable empty or not defined.}"
+
+export COSIGN_EXPERIMENTAL=1
+K8S_SIGSTORE_CLI=./kubectl-sigstore
+
+echo "Signing a sample yaml using Keyless..."
+
+cat << EOS > sample-configmap.yaml
+kind: ConfigMap 
+apiVersion: v1 
+metadata:
+  name: example-configmap 
+data:
+  key: val
+EOS
+
+$K8S_SIGSTORE_CLI sign -a sha="$GIT_HASH" -a run_id="$GITHUB_RUN_ID" -a run_attempt="$GITHUB_RUN_ATTEMPT" --image $TEST_IMAGE sample-configmap.yaml
