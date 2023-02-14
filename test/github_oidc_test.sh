ea00eea2dea6c9c23e7ba125fa8835f7e4b8089f
hirokuni-kitahara <hirokuni.kitahara1@ibm.com>
2023-02-14 14:29:00 +0900

    update github oidc test script
    Signed-off-by: hirokuni-kitahara <hirokuni.kitahara1@ibm.com>

--- test/github_oidc_test.sh
+++ test/github_oidc_test.sh
@@ -38,4 +38,5 @@ data:
 EOS
 
 
-$K8S_SIGSTORE_CLI sign --tarball=no -a sha="$GIT_HASH" -a run_id="$GITHUB_RUN_ID" -a run_attempt="$GITHUB_RUN_ATTEMPT" --image $TEST_IMAGE sample-configmap.yaml
+# $K8S_SIGSTORE_CLI sign --tarball=no -a sha="$GIT_HASH" -a run_id="$GITHUB_RUN_ID" -a run_attempt="$GITHUB_RUN_ATTEMPT" --image $TEST_IMAGE sample-configmap.yaml
+$K8S_SIGSTORE_CLI sign --tarball=no sample-configmap.yaml
